c3474eb8dc4d144a5637178e7fde95cff06739d8
nealthom <nealthom@gmail.com>
2020-04-23 21:22:33 -0500

    Init commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+require("./db/mongoose");
+
+const userRouter = require("./routes/user");
+const taskRouter = require("./routes/task");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
